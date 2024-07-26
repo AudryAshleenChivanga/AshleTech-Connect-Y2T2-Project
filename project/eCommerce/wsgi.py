d260ee4b3e97b0ff82cb50b63f8dddcc51b3f48a
AudryAshleenChivanga <a.chivanga@alustudent.com>
2024-07-26 21:23:42 +0200

    Basic styling articles

--- project/eCommerce/wsgi.py
+++ project/eCommerce/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for eCommerce project.
+WSGI config for AshleTech Connect project for eCommerce Sexual Reproductive Health project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
